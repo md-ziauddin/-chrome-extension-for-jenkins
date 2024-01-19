bb5fc5786abfe1fab821ac757e003d69fb56e31e
MD Ziauddin <md.ziauddin02@gmail.com>
2024-01-19 21:45:07 +0530

    The button is working

--- content.js
+++ content.js
@@ -1,9 +1,17 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "getBranchName") {
-    var branchElement = document.querySelector(
-      ".your-gitlab-div-class .branch-name-class"
-    );
-    var branchName = branchElement ? branchElement.innerText.trim() : "";
-    sendResponse({ branchName: branchName });
+    var branchButton = document.querySelector(".js-source-branch-copy");
+
+    if (branchButton) {
+      // Get the branch name from the data-clipboard-text attribute
+      var branchName = branchButton.getAttribute("data-clipboard-text");
+
+      sendResponse({ branchName: branchName });
+    } else {
+      sendResponse({ branchName: "" }); // Return an empty string if the button is not found
+    }
+
+    // Return true to indicate that sendResponse will be called asynchronously
+    return true;
   }
 });
--- popup.js
+++ popup.js
@@ -1,19 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
   var openJenkinsButton = document.getElementById("openJenkinsButton");
+
   openJenkinsButton.addEventListener("click", function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       var activeTab = tabs[0];
-      chrome.tabs.sendMessage(
-        activeTab.id,
-        { action: "getBranchName" },
-        function (response) {
-          if (response && response.branchName) {
-            var jenkinsUrl = "https://your-jenkins-url.com/job/your-job-name/";
-            var urlWithBranch = jenkinsUrl + "branch/" + response.branchName;
-            chrome.tabs.create({ url: urlWithBranch });
+      if (activeTab) {
+        chrome.tabs.sendMessage(
+          activeTab.id,
+          { action: "getBranchName" },
+          function (response) {
+            if (chrome.runtime.lastError) {
+              console.error(chrome.runtime.lastError.message);
+            } else if (response && response.branchName) {
+              var jenkinsUrl = "https://jenkins.com/controller9/jobs/";
+              var urlWithBranch =
+                jenkinsUrl + encodeURIComponent(response.branchName);
+
+              // Open the Jenkins URL in a new tab
+              chrome.tabs.create({ url: urlWithBranch });
+            } else {
+              console.error(
+                "Error: Unable to get branch name from content script."
+              );
+            }
           }
-        }
-      );
+        );
+      } else {
+        console.error("Error: No active tab found.");
+      }
     });
   });
 });
